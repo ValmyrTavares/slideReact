5a02fe1faaceabe88a4ba4ea16123038907e024f
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-02-15 23:10:48 -0300

    Botões funcionando

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,14 +1,31 @@
 import React from 'react'
 
 function Slide({slides}) {
+    const [active, setActive] = React.useState(0)
+    const [position, setPosition] = React.useState(0)
+    const contentRef = React.useRef();
+    console.log(contentRef)
+
+    function slidePrev(){
+      const{width} = contentRef.current.getBoundingClientRect();      
+        setPosition(position-width)
+    }
+    function slideNext(){
+        const{width} = contentRef.current.getBoundingClientRect();      
+        setPosition(position + width)
+    }
     return (
         <section className='container'>
-            <div className="content">
+            <div 
+            ref={contentRef}
+            className="content"
+            style={{transform: `translateX(${position}px)`}}
+            >
             {slides.map(item => <div key={slides.id} className="item">{item.text}</div>)}               
             </div>       
             <nav className="nav">
-                <button> Anterior</button>
-                <button>Seguinte</button>
+                <button onClick={slidePrev}> Anterior</button>
+                <button onClick={slideNext}>Seguinte</button>
             </nav>
         </section>
     )
